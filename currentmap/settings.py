4336c65e31ff335a8028c81b2087915159314deb
Clément NICOLAS <racacax@gmail.com>
2023-01-08 00:00:36 +0100

    fix leak

--- currentmap/settings.py
+++ currentmap/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ig#kogjkd%ftbsg%ps=+x_c)31d*femnm26y#7qpa2#*6y5ap3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
