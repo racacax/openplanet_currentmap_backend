eb3d24167c50ad3d3f003143483c1962734c1051
Clément NICOLAS <racacax@gmail.com>
2023-01-08 00:01:22 +0100

    add domain

--- currentmap/settings.py
+++ currentmap/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "currentmap.racacax.gq"]
 
 
 # Application definition
